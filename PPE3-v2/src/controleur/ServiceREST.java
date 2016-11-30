c2d9e9dcb66e7e43cb6fa1f25ee172d5f527e9d8
soufian <soufian@LAPTOP-E52NI2EM>
2016-11-30 22:23:58 +0100

    première màj serviceREST

--- PPE3-v2/src/controleur/ServiceREST.java
+++ PPE3-v2/src/controleur/ServiceREST.java
@@ -1,10 +1,17 @@
 package controleur;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import modele.Utilisateurs;
 
 @Path("dto")
 public class ServiceREST {
@@ -12,6 +19,15 @@ public class ServiceREST {
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public MessageDTO getDTO(@QueryParam("login")String login, @QueryParam("password")String password){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jdbc");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		List<Utilisateurs> utilisateur = em.createNativeQuery("SELECT * FROM UTILISATEURS", Utilisateurs.class).getResultList();
+		
+		em.getTransaction().commit();
+		em.close();
+		
 		
 		
 		MessageDTO message = new MessageDTO("Bienvenue "+login, "Administrateur");
