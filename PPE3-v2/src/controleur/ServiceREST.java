cea1c829e4fa1b4463aab268c48604e679f36e9e
soufian <soufian@LAPTOP-E52NI2EM>
2016-11-30 23:01:27 +0100

    v1.0 fonctionnelle du Serveur

--- PPE3-v2/src/controleur/ServiceREST.java
+++ PPE3-v2/src/controleur/ServiceREST.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import modele.Roles_utilisateurs;
 import modele.Utilisateurs;
 
 @Path("dto")
@@ -19,6 +20,7 @@ public class ServiceREST {
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public MessageDTO getDTO(@QueryParam("login")String login, @QueryParam("password")String password){
+		MessageDTO message=null;
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jdbc");
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
@@ -28,9 +30,22 @@ public class ServiceREST {
 		em.getTransaction().commit();
 		em.close();
 		
+		for(Utilisateurs iter:utilisateur){
+			if(iter.getEmail().equals(login)){
+				if(iter.getPassword().equals(password)){
+					String roles=null;
+					for(Roles_utilisateurs iter2: iter.getRoles()){
+						roles += iter2.getRole().getRole()+", ";
+					}
+					message=new MessageDTO(iter.getNom()+" "+iter.getPrenom(),roles);
+				}
+				else
+					message=new MessageDTO("Erreur lors de la saisie du mot de passe !","");
+			}
+			else
+				message=new MessageDTO("Email inconnue !","");
+		}
 		
-		
-		MessageDTO message = new MessageDTO("Bienvenue "+login, "Administrateur");
 		return message;
 	}
 	
