270b90a73a676d31e774f6887635da9bb2864ea9
invite <invite@linux18.btssio>
2016-12-12 16:55:25 +0100

    16:55

--- PP3-v2-JOURNALISATEUR/src/controleur/Journalisateur.java
+++ PP3-v2-JOURNALISATEUR/src/controleur/Journalisateur.java
@@ -51,23 +51,18 @@ public class Journalisateur extends HttpServlet {
 	public void consommer() throws IOException, TimeoutException {
 		
 		ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("rabbitmq");
-	    connection = factory.newConnection();
-	    channel = connection.createChannel();
-	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);	    
-	    
-	    Consumer consumer = new DefaultConsumer(channel) {
-	        @Override
-	        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-	            throws IOException {
-	       
-	          message = new String(body, "UTF-8");
-	          System.out.println("MESSAGE =======> " + message);
-	          journaliser();
-	        }
-	      };
-	      System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	      channel.basicConsume(QUEUE_NAME, true, consumer);
+ 	    factory.setHost("rabbitmq");
+ 	    Connection connection = factory.newConnection();
+ 	    Channel channel = connection.createChannel();
+ 	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);	    
+ 	    Consumer consumer = new DefaultConsumer(channel) {
+ 	        @Override
+ 	        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+ 	            throws IOException {
+ 	          message = new String(body, "UTF-8");
+ 	        }
+ 	      };
+channel.basicConsume(QUEUE_NAME, true, consumer);
 	}
 	
 	private void journaliser() {
