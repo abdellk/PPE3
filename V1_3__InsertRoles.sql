f42a70be25f591fef516d38e0cda70e9105f97c4
Soufian AIT TIRITE <aittiritesoufian@users.noreply.github.com>
2017-01-01 11:33:47 +0100

    Delete V1_3__InsertRoles.sql

--- V1_3__InsertRoles.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "DERBY"."ROLES" (DERNIERE_MISE_A_JOUR,DESCRIPTION,MODIFIER_PAR,ROLE) VALUES (CURRENT_TIMESTAMP,'gérer le magasin, les accès et consulter les indicateurs','Soufian AIT TIRITE','Administrateur');
-INSERT INTO "DERBY"."ROLES" (DERNIERE_MISE_A_JOUR,DESCRIPTION,MODIFIER_PAR,ROLE) VALUES (CURRENT_TIMESTAMP,'rédigier, modifier, supprimer, publier des annonces et vendre des guitares','Soufian AIT TIRITE','Magasin');
-INSERT INTO "DERBY"."ROLES" (DERNIERE_MISE_A_JOUR,DESCRIPTION,MODIFIER_PAR,ROLE) VALUES (CURRENT_TIMESTAMP, 'rédigier, modifier et supprimer des annonces','Soufian AIT TIRITE','Annonceur');
-
