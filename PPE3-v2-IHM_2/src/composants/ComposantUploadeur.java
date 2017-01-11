71b6e0f301a57d5468f947c4942dbd5a93c647bc
invite <invite@linux18.btssio>
2017-01-11 11:38:21 +0100

    11:38

--- PPE3-v2-IHM_2/src/composants/ComposantUploadeur.java
+++ PPE3-v2-IHM_2/src/composants/ComposantUploadeur.java
@@ -35,25 +35,25 @@ public class ComposantUploadeur {
 		try {
 				//fileContent = new Scanner(file.getInputStream()).useDelimiter("\\A").next();
 				//System.out.println(fileContent);
-				file.write("/tmp/" + file.getSubmittedFileName());
-				byte[] flux = Files.readAllBytes(Paths.get("/tmp/"+file.getSubmittedFileName()));
-				boolean statut = transfert(new String(flux, Charset.defaultCharset()));
-				if (statut) resultat = "Le chargement a réussi";
-				else resultat = "échec du transfert du fichier";
-				File lefichier = new File("/tmp/"+file.getSubmittedFileName());
-				lefichier.delete();
+				file.write("/tmp/" + file.getSubmittedFileName());//enregistrement du fichier sur le serveur local dans le répertoire /tmp
+				byte[] flux = Files.readAllBytes(Paths.get("/tmp/"+file.getSubmittedFileName()));//conversion du fichier xml en bytecode
+				boolean statut = transfert(new String(flux, Charset.defaultCharset()));//transfert du fichier convertis en chaîne de caractère vers le serveur CREATEUR UTILISATEUR
+				if (statut) resultat = "Le chargement a réussi";//si le transfert c'est correctement effectué le message de résultat sera celui-ci
+				else resultat = "échec du transfert du fichier";//sinon ce sera celui-ci
+				File lefichier = new File("/tmp/"+file.getSubmittedFileName());//chargement du fichier précédemment stocké localement
+				lefichier.delete();//suppression du fichier local
 		} catch (IOException e) { e.printStackTrace();
-			resultat = "échec du chargement";
+			resultat = "échec du chargement";// message d'erreur en cas d'échec lors du traitement du fichier xml
 		}
-		return "confirmation.xhtml";
+		return "confirmation.xhtml";// page vers laquelle devra rediriger le serveur après avoir cliquer sur le bouton "Chargement du fichier"
 	}
 	
 	private boolean transfert(String xml) {
-		Client client = ClientBuilder.newClient();
-		WebTarget cible = client.target(UriBuilder.fromPath("http://tomcatcreateurutilisateur:8080/PPE3v2CREATEURUTILISATEUR/"));
-		WebTarget ciblefinale = cible.path("xml");
-		ciblefinale.request(MediaType.TEXT_PLAIN_TYPE).post(Entity.entity(xml, MediaType.TEXT_PLAIN));
-		return true;
+		Client client = ClientBuilder.newClient();//création d'un client REST
+		WebTarget cible = client.target(UriBuilder.fromPath("http://tomcatcreateurutilisateur:8080/PPE3v2CREATEURUTILISATEUR/"));//connexion au serveur distant
+		WebTarget ciblefinale = cible.path("xml");//PATH ciblé dans le serveur
+		ciblefinale.request(MediaType.TEXT_PLAIN_TYPE).post(Entity.entity(xml, MediaType.TEXT_PLAIN));//fichier xml envoyer en chaîne de caractère envoyé par méthode HTTP POST
+		return true;//renvoi TRUE si aucune erreur n'est sûrvenue dans l'exécution de la méthode
 	}
 
 	public String getFileContent() {
