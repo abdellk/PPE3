000e9357afddf9af52c69db8eddc250b4b63e11c
invite <invite@linux18.btssio>
2017-01-18 15:01:29 +0100

    15:01

--- PPE3-v2-CREATEUR_UTILISATEUR/src/outils/Resultat.java
+++ PPE3-v2-CREATEUR_UTILISATEUR/src/outils/Resultat.java
@@ -24,17 +24,33 @@ public class Resultat {
 		return employees;
 	}
 	
-	public void enregistrer() {
-		EntityManager em = FournisseurDePersistance.getInstance().fournir();//connexion à la base de donnée
+	public boolean enregistrer() {
+		/*EntityManager em = FournisseurDePersistance.getInstance().fournir();//connexion à la base de donnée
 		em.getTransaction().begin();
 		System.out.println("TAILLE : " + employees.size());
 		Role rolemagasin = (Role) em.createNativeQuery("select * from role where role='magasin'", Role.class).getSingleResult();
 		for(Utilisateur obj : employees) {
 			System.out.println(obj);
 			rolemagasin.ajoutUtilisateur(obj);//persistence de chacun des utilisateurs en les ajoutants au role magasin
+			
 		}
 		em.getTransaction().commit();
 		em.close();
-		FournisseurDePersistance.getInstance().detruire();
+		FournisseurDePersistance.getInstance().detruire();*/
+		EntityManager em = null;
+		try {
+				em = FournisseurDePersistance.getInstance().fournir();
+				em.getTransaction().begin();
+				Role rolemagasin = (Role) em.createNativeQuery("select * from role where role='magasin'", Role.class).getSingleResult();
+				for(Utilisateur ref : employees) {
+					rolemagasin.ajoutUtilisateur(ref);
+					em.persist(ref);
+				}
+				em.getTransaction().commit();
+		return true;		
+		} catch (Exception e) {e.printStackTrace(); return false; }
+		finally {
+			em.close();
+		}	
 	}
 }
